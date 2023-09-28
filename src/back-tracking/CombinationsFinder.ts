98679ef1a71204e42cf7304669447785efba3c01
Anh Tran <anh.q.tran@eastagile.com>
2023-09-28 23:16:41 +0700

    feat: add back tracking CombinationsFinder

---
+++ src/back-tracking/CombinationsFinder.ts
@@ -0,0 +1,35 @@
+class CombinationsFinder<T> {
+  private set: Array<T>;
+  private k: number;
+  private result: Array<Array<T>> = [];
+  private x: number[] = [];
+
+  constructor(set: Array<T>, k: number) {
+    this.set = set;
+    this.k = k;
+  }
+
+  public findCombinations(): Array<Array<T>> {
+    this.exec(0);
+    return this.result;
+  }
+
+  private exec(i: number): void {
+    for (
+      let j = this.x[i - 1] + 1 || 0;
+      j <= this.set.length - this.k + i;
+      j++
+    ) {
+      this.x[i] = j;
+      if (i === this.k - 1) {
+        this.result.push(this.x.map((e) => this.set[e]));
+      } else {
+        this.exec(i + 1);
+      }
+    }
+  }
+}
+
+let finder = new CombinationsFinder([1, 2, 3, 4, 5], 3);
+let combinations = finder.findCombinations();
+console.log(combinations);
